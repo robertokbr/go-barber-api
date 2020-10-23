3c65d69b8656dab2a92b0f21ffa35600fb62a496
robertokbr <robertojuniordev@gmail.com>
2020-10-23 12:11:48 -0300

    Add Dependency Injection AuthenticateUserService

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import { container } from 'tsyringe';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
-  const authenticate = new AuthenticateUserService();
+
+  const authenticate = container.resolve(AuthenticateUserService);
+
   const { user, token } = await authenticate.execute({
     password,
     email,
   });
 
-  delete user.password;
+  user.password = '';
 
   return response.json({ user, token });
 });
--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,38 +1,51 @@
-import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '@modules/users/infra/typeorm/entities/User';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 interface Request {
   email: string;
   password: string;
 }
+
 interface Response {
   user: User;
   token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
+  private userRepository: IUsersRepository;
+
+  constructor(
+    @inject('UsersRepository')
+    userRepository: IUsersRepository,
+  ) {
+    this.userRepository = userRepository;
+  }
+
   public async execute({ email, password }: Request): Promise<Response> {
-    const userRepository = getRepository(User);
-    const user = await userRepository.findOne({
-      where: { email },
-    });
+    const user = await this.userRepository.findByEmail(email);
+
     if (!user) {
       throw new AppError('Email or password incorrect', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
+
     if (!passwordMatched) {
       throw new AppError('Email or password incorrect', 401);
     }
+
     const { secret, expiresIn } = authConfig.jwt;
     const token = sign({}, secret, {
       subject: user.id,
       expiresIn,
     });
+
     return { user, token };
   }
 }
