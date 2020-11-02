13ab929ba30f25aa59078633d211b7b51d67dc0a
robertokbr <robertojuniordev@gmail.com>
2020-11-02 12:18:00 -0300

    Apply on server tsyringe

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -6,6 +6,7 @@ import routes from '@shared/infra/http/routes';
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 app.use(cors());
