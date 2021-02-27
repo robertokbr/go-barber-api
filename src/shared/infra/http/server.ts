56d91da8cd313f88724bf99fde7195ef7c3b094d
robertokbr <robertojuniordev@gmail.com>
2021-02-27 17:30:22 -0300

    Update server config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+
 import routes from '@shared/infra/http/routes';
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
@@ -9,9 +10,11 @@ import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+const PORT = process.env.PORT || 3333;
+
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -27,6 +30,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     message: 'Internal Server error',
   });
 });
-app.listen(3333, () => {
-  console.log(`server started on port 3333 at `);
+
+app.listen(PORT, () => {
+  console.log(`server connected on port ${PORT} 🚪`);
 });
