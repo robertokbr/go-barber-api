2fe95cf710891f2be77988ee790e2415fd6a1228
robertokbr <robertojuniordev@gmail.com>
2021-03-03 21:56:46 -0300

    Add UpdateProfileService and spec test

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -33,6 +33,7 @@ describe('UpdateProfileService', () => {
     });
 
     expect(user.name).toEqual('person2');
+    expect(user.email).toEqual('test2@gmail.com');
   });
 
   it('Should not be able to updated e-mail using an "already in use" e-mail', async () => {
@@ -59,26 +60,55 @@ describe('UpdateProfileService', () => {
     ).rejects.toBeInstanceOf(AppError);
   });
 
-  it('Should not be able to ', async () => {
-    const userOne = await fakeUsersRepository.create({
+  it('Should be able to update user password', async () => {
+    const user = await fakeUsersRepository.create({
       email: 'test@gmail.com',
       name: 'person',
       password: '123456',
     });
 
-    await fakeUsersRepository.create({
-      email: 'test2@gmail.com',
+    await updateProfileService.execute({
+      user_id: user.id,
+      email: 'test@gmail.com',
+      name: 'person',
+      old_password: '123456',
+      password: '654321',
+    });
+
+    expect(user.password).toBe('654321');
+  });
+
+  it('Should not be able to update user password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      email: 'test@gmail.com',
       name: 'person',
       password: '123456',
     });
 
     await expect(
       updateProfileService.execute({
-        user_id: userOne.id,
-        email: 'test2@gmail.com',
-        name: 'person2',
+        user_id: user.id,
+        email: 'test@gmail.com',
+        name: 'person',
         password: '654321',
-        old_password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to update user password with a invalid old password', async () => {
+    const user = await fakeUsersRepository.create({
+      email: 'test@gmail.com',
+      name: 'person',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        email: 'test@gmail.com',
+        name: 'person',
+        old_password: 'invalid-password',
+        password: '654321',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -8,8 +8,8 @@ interface Request {
   user_id: string;
   name: string;
   email: string;
-  password: string;
-  old_password: string;
+  password?: string;
+  old_password?: string;
 }
 
 @injectable()
@@ -22,7 +22,13 @@ class UpdateProfileService {
     private hashProvider: IHashProvider,
   ) {}
 
-  public async execute({ user_id, email, name }: Request): Promise<User> {
+  public async execute({
+    user_id,
+    email,
+    name,
+    password,
+    old_password,
+  }: Request): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
@@ -32,11 +38,26 @@ class UpdateProfileService {
     const userWithProvidedEmail = await this.usersRepository.findByEmail(email);
 
     if (userWithProvidedEmail && userWithProvidedEmail.id !== user.id) {
-      throw new AppError('Email already in user');
+      throw new AppError('Email already in use');
     }
 
     user.name = name;
     user.email = email;
+
+    if (password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password!,
+        user.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError(
+          'You must provide a valid "old password" to update your password',
+        );
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
 
     return this.usersRepository.save(user);
   }
