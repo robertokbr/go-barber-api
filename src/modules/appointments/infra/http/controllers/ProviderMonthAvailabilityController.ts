d7e44fe6cede061d7fd4001237c79dbbf50d9e9d
robertokbr <robertojuniordev@gmail.com>
2021-03-06 23:17:22 -0300

    Add ProviderMonthAvailabilityController

---
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,26 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+
+class ProviderMonthAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+
+    const { month, year } = request.query;
+
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
+
+    const users = await listProviderMonthAvailability.execute({
+      provider_id,
+      month: Number(month),
+      year: Number(year),
+    });
+
+    return response.json(users);
+  }
+}
+
+export default ProviderMonthAvailabilityController;
