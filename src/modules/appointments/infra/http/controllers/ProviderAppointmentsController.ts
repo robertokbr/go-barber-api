63e837a2d01da6a925c2b80f59a5e39a3efe24d4
robertokbr <robertojuniordev@gmail.com>
2021-03-07 00:07:11 -0300

    Add provider appointments index into the route

---
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { day, year, month } = request.params;
+
+    const provider_id = request.user.id;
+
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointmentsService,
+    );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day: Number(day),
+      year: Number(year),
+      month: Number(month),
+    });
+
+    return response.json(appointments);
+  }
+}
+
+export default ProviderAppointmentsController;
--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
