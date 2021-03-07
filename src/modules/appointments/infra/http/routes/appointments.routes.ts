a2165b8b596b05f0600490a135fe8e072546551a
robertokbr <robertojuniordev@gmail.com>
2021-03-06 23:18:19 -0300

    Update appointment route

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,17 +1,13 @@
 import { Router } from 'express';
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController AppointmentsController';
 
 const appointmentsRouter = Router();
+
 const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
-// appointmentsRouter.get('/', async (request, response) => {
-//   console.log(request.user);
-
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
 
 appointmentsRouter.post('/', appointmentsController.create);
 
