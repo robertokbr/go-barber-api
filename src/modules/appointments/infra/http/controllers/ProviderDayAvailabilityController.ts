a060d84ae18369e07a33ea282b11ee9e86f8e5fe
robertokbr <robertojuniordev@gmail.com>
2021-03-06 23:17:01 -0300

    Add ProviderDayAvailabilityController

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,27 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+
+    const { day, month, year } = request.query;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const users = await listProviderDayAvailability.execute({
+      provider_id,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
+    });
+
+    return response.json(users);
+  }
+}
+
+export default ProviderDayAvailabilityController;
