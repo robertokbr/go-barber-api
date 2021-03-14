8ffcc1b82f7f088691c8c9d177bd5703b9a8984b
robertokbr <robertojuniordev@gmail.com>
2021-03-13 21:19:24 -0300

    Add prod storage provider

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import aws, { S3 } from 'aws-sdk';
+import storage from '@config/storage';
+import mime from 'mime';
+
+class S3StorageProvider implements IStorageProvider {
+  private cliente: S3;
+
+  constructor() {
+    this.cliente = new aws.S3({
+      region: 'us-east-1',
+    });
+  }
+
+  public async saveFile(fileName: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, fileName);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    await this.cliente
+      .putObject({
+        Bucket: storage.bucket,
+        ACL: 'public-read',
+        Key: fileName,
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return fileName;
+  }
+
+  public async deleteFile(fileName: string): Promise<void> {
+    await this.cliente
+      .deleteObject({
+        Bucket: storage.bucket,
+        Key: fileName,
+      })
+      .promise();
+  }
+}
+
+export default S3StorageProvider;
---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe';
+
+import storage from '@config/storage';
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers[storage.driver],
+);
