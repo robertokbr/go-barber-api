fcb28d8520e877006046f95e2c10d1304c609e12
robertokbr <robertojuniordev@gmail.com>
2021-03-16 21:31:19 -0300

    Update configs

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,13 @@
+interface IAuthConfig {
+  jwt: {
+    secret: string;
+    expiresIn: string;
+  };
+}
+
 export default {
   jwt: {
-    secret: process.env.APP_SECRET,
+    secret: process.env.APP_SECRET || 'secret',
     expiresIn: '1d',
   },
-};
+} as IAuthConfig;
