d956505072d176b1d3fbd4f7fabd65dbb1f7e448
robertokbr <robertojuniordev@gmail.com>
2021-03-26 17:38:35 -0300

    Update server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -17,21 +17,21 @@ const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
-app.use(rateLimiter);
 app.use('/files', express.static(uploadConfig.uploadFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  console.log(err);
+
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
     });
   }
-
-  console.log(err);
 
   return response.status(500).json({
     status: 'error',
