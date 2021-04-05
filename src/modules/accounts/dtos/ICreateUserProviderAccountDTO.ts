9f26dcf57364c298c82e474c3b332364177bedc5
robertokbr <robertojuniordev@gmail.com>
2021-04-05 19:14:36 -0300

    Change ICreateUserProviderAccountDTO

--- src/modules/accounts/dtos/ICreateUserProviderAccountDTO.ts
+++ src/modules/accounts/dtos/ICreateUserProviderAccountDTO.ts
@@ -1,6 +1,9 @@
-import User from '@modules/users/infra/typeorm/entities/User';
-
 export default interface ICreateUserProviderAccountDTO {
   account_type_id: number;
-  user: User;
+  user: {
+    id: string;
+    email: string;
+    name: string;
+    password: string;
+  };
 }
